d63be7eea7d238d8d60ab3ce63f8630bd5235e45
Ittipat Pattum <nampu@finema.co>
2025-11-12 21:45:20 +0700

    Update Banner.vue

--- app/components/top/Banner.vue
+++ app/components/top/Banner.vue
@@ -7,9 +7,16 @@
             <div class="text-base pl-4">Get 5% Off your first order,</div>
             <div class="text-base font-bold text-primary-orange underline pl-1 cursor-pointer" v-on:click="onClicked('Promo: ORDER5')">Promo: ORDER5</div>
             <div class="flex-1"></div>
+            <Icon name="ri:map-pin-2-fill" size="24px"></Icon>
+            <div class="text-base pl-4">Regent Street,</div>
+            <div class="text-base pl-1">A4</div>
+            <div class="text-base pl-1">A4201, London</div>
+            <div class="w-4"></div>
+            <div class="text-base text-primary-orange underline cursor-pointer" v-on:click="onClicked('Promo: ORDER5')">Change Location</div>
+            <div class="w-14"></div>
             <div class="pl-5 pr-4 h-full bg-primary-green rounded-bl-2xl flex justify-center items-center cursor-pointer relative" v-on:click="onClicked('Promo: ORDER5')">
                 <Icon name="ri:shopping-basket-fill" style="color: white" size="32px"></Icon>
-                <Icon name="ri:checkbox-circle-fill" style="color: white" size="16px" class="absolute right-3 bottom-3 z-10"></Icon>
+                <Icon name="ri:checkbox-circle-fill" style="color: white" size="24px" class="absolute right-2 bottom-2 z-10"></Icon>
             </div>
             <div class="px-4 h-full bg-primary-green flex justify-center items-center text-[16px] font-semibold text-white border-l">23 Items</div>
             <div class="px-4 h-full bg-primary-green flex justify-center items-center text-[16px] font-semibold text-white border-x">GBP 79.89</div>

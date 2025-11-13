fad05ff3aa0a8c57ed4e00d2d90a3507cde17c6d
Ittipat Pattum <nampu@finema.co>
2025-11-13 17:07:35 +0700

    save

--- app/components/home/TopCard.vue
+++ app/components/home/TopCard.vue
@@ -1,4 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: "handle-submitted", value: string): void;
+}>();
+
+const inputValue = useState<string>(uuidv4(), () => "");
+function submitted() {
+  emit("handle-submitted", inputValue.value);
+}
+</script>
 
 <template>
   <div class="h-[610px] bg-top-gray border border-blk-op-20 rounded-2xl flex flex-col justify-center items-start pl-10">
@@ -6,6 +15,10 @@
     <div class="text-primary-black text-[54px] font-semibold">Feast Your Senses,</div>
     <div class="text-primary-orange text-[54px] font-semibold">Fast and Fresh</div>
     <div class="text-primary-black text-[13px]">Enter a postcode to see what we deliver</div>
+    <div class="bg-white h-14 w-[480px] border border-blk-op-40 flex items-center rounded-full">
+      <input v-model="inputValue" type="text" placeholder="e.g. EC4R 3TE" class="flex-1 border-none outline-none px-7" v-on:keyup.enter="submitted()" />
+      <button type="submit" class="h-14 bg-primary-orange rounded-full px-16 text-[16px] font-bold text-white cursor-pointer" v-on:click="submitted()">Search</button>
+    </div>
   </div>
 </template>
 
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <HomeTopCard></HomeTopCard>
+  <HomeTopCard v-on:handle-submitted="(inputValue: string) => onClicked(inputValue)"></HomeTopCard>
 </template>
 
 <style scoped></style>

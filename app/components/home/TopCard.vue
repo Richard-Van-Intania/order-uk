c5e2f54da893baf49582d49c3f08a92b2999c31d
Ittipat Pattum <nampu@finema.co>
2025-11-13 15:41:30 +0700

    save

--- app/components/home/TopCard.vue
+++ app/components/home/TopCard.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="h-[610px] bg-top-gray border border-blk-op-20 rounded-2xl">dddd</div>
+  <div class="h-[610px] bg-top-gray border border-blk-op-20 rounded-2xl flex flex-col justify-center items-start pl-10">
+    <div class="text-primary-black">Order Restaurant food, takeaway and groceries.</div>
+    <div class="text-primary-black text-[54px] font-semibold">Feast Your Senses,</div>
+    <div class="text-primary-orange text-[54px] font-semibold">Fast and Fresh</div>
+    <div class="text-primary-black text-[13px]">Enter a postcode to see what we deliver</div>
+  </div>
 </template>
 
 <style scoped></style>

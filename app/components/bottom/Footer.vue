7ecd849754097975770a184a1952a431c2d3c46b
Ittipat Pattum <nampu@finema.co>
2025-11-13 18:59:57 +0700

    save

--- app/components/bottom/Footer.vue
+++ app/components/bottom/Footer.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>dddd</div>
+  <div class="bg-footer-gray p-14 flex items-center">dddd</div>
 </template>
 
 <style scoped></style>
--- app/components/bottom/Terms.vue
+++ app/components/bottom/Terms.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>dddd</div>
+  <div class="h-[72px] px-14 bg-primary-black flex items-center text-white text-[15px]">Order.uk Copyright 2024, All Rights Reserved.</div>
 </template>
 
 <style scoped></style>

2dc0efb5a80c91f51a125aead29b60f37b5e039e
Ittipat Pattum <nampu@finema.co>
2025-11-13 18:51:20 +0700

    save

--- app/components/home/TopCard.vue
+++ app/components/home/TopCard.vue
@@ -13,8 +13,8 @@ function submitted() {
   <div class="h-[610px] bg-top-gray border border-blk-op-20 rounded-2xl flex flex-col justify-center items-start pl-10">
     <div class="text-primary-black">Order Restaurant food, takeaway and groceries.</div>
     <div class="text-primary-black text-[54px] font-semibold">Feast Your Senses,</div>
-    <div class="text-primary-orange text-[54px] font-semibold">Fast and Fresh</div>
-    <div class="text-primary-black text-[13px]">Enter a postcode to see what we deliver</div>
+    <div class="text-primary-orange text-[54px] font-semibold leading-none">Fast and Fresh</div>
+    <div class="text-primary-black text-[13px] pt-10 pb-2">Enter a postcode to see what we deliver</div>
     <div class="bg-white h-14 w-[480px] border border-blk-op-40 flex items-center rounded-full">
       <input v-model="inputValue" type="text" placeholder="e.g. EC4R 3TE" class="flex-1 border-none outline-none px-7" v-on:keyup.enter="submitted()" />
       <button type="submit" class="h-14 bg-primary-orange rounded-full px-16 text-[16px] font-bold text-white cursor-pointer" v-on:click="submitted()">Search</button>

e697bca2765f74f2a0539067d9c42fdb5c3ce8e7
Ittipat Pattum <nampu@finema.co>
2025-11-14 20:48:57 +0700

    save

--- app/components/home/about/Card.vue
+++ app/components/home/about/Card.vue
@@ -7,9 +7,9 @@ const { currentAbout, setNewAbout } = useCurrentAbout();
         <div class="flex items-center pt-24 pb-14">
             <div class="text-[32px] font-bold flex-1">Know more about us!</div>
             <HomeAboutOutlineButton label="Frequent Questions" v-bind:current="currentAbout === CurrentAbount.Frequent" v-on:handle-clicked="setNewAbout(CurrentAbount.Frequent)"></HomeAboutOutlineButton>
-            <div class="pl-1"><HomeAboutOutlineButton label="Who we are?" v-bind:current="currentAbout === CurrentAbount.Who" v-on:handle-clicked="setNewAbout(CurrentAbount.Who)"></HomeAboutOutlineButton></div>
-            <div class="pl-1"><HomeAboutOutlineButton label="Partner Program" v-bind:current="currentAbout === CurrentAbount.Partner" v-on:handle-clicked="setNewAbout(CurrentAbount.Partner)"></HomeAboutOutlineButton></div>
-            <div class="pl-1"><HomeAboutOutlineButton label="Help & Support" v-bind:current="currentAbout === CurrentAbount.Help" v-on:handle-clicked="setNewAbout(CurrentAbount.Help)"></HomeAboutOutlineButton></div>
+            <div class="pl-2"><HomeAboutOutlineButton label="Who we are?" v-bind:current="currentAbout === CurrentAbount.Who" v-on:handle-clicked="setNewAbout(CurrentAbount.Who)"></HomeAboutOutlineButton></div>
+            <div class="pl-2"><HomeAboutOutlineButton label="Partner Program" v-bind:current="currentAbout === CurrentAbount.Partner" v-on:handle-clicked="setNewAbout(CurrentAbount.Partner)"></HomeAboutOutlineButton></div>
+            <div class="pl-2"><HomeAboutOutlineButton label="Help & Support" v-bind:current="currentAbout === CurrentAbount.Help" v-on:handle-clicked="setNewAbout(CurrentAbount.Help)"></HomeAboutOutlineButton></div>
         </div>
         <div class="h-[526px] bg-white rounded-2xl pl-8 pt-8 pb-8 pr-7 flex"></div>
     </div>
